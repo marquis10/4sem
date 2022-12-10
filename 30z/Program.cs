e7bbada08a6a3cd3bde2c3110e93dbb934681ab8
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-10 17:27:50 +0500

    30z1

--- 30z/Program.cs
+++ 30z/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task 30
+
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 2); // [0, 1]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
