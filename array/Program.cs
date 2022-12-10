12b54193ca4d16b470a07f2c47b1e467c9d67abc
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-10 16:57:34 +0500

    array function

--- array/Program.cs
+++ array/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int f(int a, int b=10)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+// return возвращает значение либо в консоль, либо в переменную
+// return завершает работу функции
+// Сколько аргументов мы передаем, столько и принимаем. НО!!!
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a));
