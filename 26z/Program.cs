247c39ed6cb26dd2e6eb82d3b609a856c96f77c7
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-10 17:21:26 +0500

    26z1

--- 26z/Program.cs
+++ 26z/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (n > 0)
+{
+    n /= 10;
+    count++;
+}
+Console.WriteLine($"Количество символов в числе: {count}");
