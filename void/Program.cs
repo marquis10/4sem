303e9ce537933fa41c6c2172fa30564598d769ef
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-10 17:03:55 +0500

    void

---
+++ void/Program.cs
@@ -0,0 +1,13 @@
+﻿// Void
+
+void f(ref int a)
+{
+    a += 5;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+f(ref a);
+Console.WriteLine(a);
