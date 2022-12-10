532df6447efbccbb50c719934c07748a819dec8c
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-10 16:16:06 +0500

    19z

---
+++ 19z/Program.cs
@@ -0,0 +1,13 @@
+﻿// task 19
+
+Console.Clear();
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+int count = number.Length - 1;
+double newNumber = 0;
+for (int i = count; i >= 0; i--)
+{
+    newNumber = newNumber + int.Parse(number[i].ToString()) * Math.Pow(10, i);
+}
+
+Console.WriteLine(number == newNumber.ToString());
