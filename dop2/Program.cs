d91ac9450945c78b20cccb06a52ef21f324532d8
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-12 00:27:21 +0500

    3sem

--- dop2/Program.cs
+++ dop2/Program.cs
@@ -1,15 +1,15 @@
-﻿int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
-string[] input = Console.ReadLine().Split(" ");
-for (int i = 0; i < input.Length; i++)
-    array[i] = int.Parse(input[i].ToString());
-
-int maxSumma = 0;
-for (int i = 1; i < array.Length - 1; i++)
+﻿int n = Convert.ToInt32(Console.ReadLine()); // захват вводимого значения
+int[] array = new int[n]; // Объявление массива с количеством элементов n
+string[] input = Console.ReadLine().Split(" "); // объявление массива строкового типа, захват значений введённых через пробел
+for (int i = 0; i < input.Length; i++)  // Проход по всем введённы значениям
+    array[i] = int.Parse(input[i].ToString()); // запись введённых значений в массив
+// сбор ягод
+int maxSumma = 0; // сохраняем максимальное количество собранных ягод
+for (int i = 1; i < array.Length - 1; i++) // Проход по 3 кустам
 {
-    int sum = array[i - 1] + array[i] + array[i + 1];
-    if (sum > maxSumma)
-        maxSumma = sum;
+    int sum = array[i - 1] + array[i] + array[i + 1]; // Сумма ягод в 3 кустах
+    if (sum > maxSumma) // Сравниваем количество ягод с максимальным
+        maxSumma = sum; // Если сумма больше, то перезаписываем максимамум
 }
 if (array[0] + array[1] + array[array.Length - 1] > maxSumma)
     maxSumma = array[0] + array[1] + array[array.Length - 1];
